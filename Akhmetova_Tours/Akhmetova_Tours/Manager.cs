731a5d09201ac8f897669fe640a9f3004a591246
Azalia1Akhmetova <aazalia75@gmail.com>
2023-09-13 21:40:29 +0500

    Сделано все, кроме картинки

---
+++ Akhmetova_Tours/Akhmetova_Tours/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Akhmetova_Tours
+{
+    internal class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
